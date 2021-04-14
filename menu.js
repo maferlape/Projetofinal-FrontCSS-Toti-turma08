8b3140372163f34be32190bd374d52d1952099fb
maferlape <maria.lares.26@gmail.com>
2021-04-14 20:50:07 -0300

    Subiendo Projeto

---
+++ menu.js
@@ -0,0 +1,9 @@
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+
+menuSection.addEventListener("click",()=>{
+    // document.body.style.overflow = show ? "hidden" : "initial"
+    menuSection.classList.toggle("on", show)
+    show = !show
+})
