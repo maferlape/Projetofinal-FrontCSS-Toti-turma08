1901e96c1b9e85708cfddd1226484c07d7e6edb7
maferlape <maria.lares.26@gmail.com>
2021-04-16 21:54:42 -0300

    Modifiocando Arquivos

--- menu.js
+++ menu.js
@@ -1,9 +1,12 @@
 let show = true;
 
 const menuSection = document.querySelector(".menu-section")
+const menutoggle = menuSection.querySelector(".menu-toggle ")
 
-menuSection.addEventListener("click",()=>{
-    // document.body.style.overflow = show ? "hidden" : "initial"
+menutoggle.addEventListener("click",()=>{
     menuSection.classList.toggle("on", show)
     show = !show
 })
+
+
+
